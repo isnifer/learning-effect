885d8934e390c4871ed2767da491624eb7c3c816
Anton Kuznetsov <isnifer@gmail.com>
2026-08-04 04:25:26 +0400

    Update CreateTodoProcedure.ts

--- packages/api/src/server/entrypoints/orpc/CreateTodoProcedure.ts
+++ packages/api/src/server/entrypoints/orpc/CreateTodoProcedure.ts
@@ -1,10 +1,22 @@
-import { os } from '@orpc/server'
+import { ORPCError, os } from '@orpc/server'
+import * as Effect from 'effect/Effect'
+import * as Match from 'effect/Match'
 import * as Schema from 'effect/Schema'
 import CreateTodo, { CreateTodoInput } from '#/server/application/usecases/CreateTodo'
 import AppRuntime from '#/server/runtime/AppRuntime'
 
 const CreateTodoProcedure = os
   .input(Schema.toStandardSchemaV1(CreateTodoInput))
-  .handler(({ input }) => AppRuntime.runPromise(CreateTodo(input)))
+  .handler(({ input }) =>
+    CreateTodo(input).pipe(
+      Effect.catch(
+        Match.valueTags({
+          TodoRepositoryError: cause =>
+            Effect.fail(new ORPCError('INTERNAL_SERVER_ERROR', { cause })),
+        })
+      ),
+      AppRuntime.runPromise
+    )
+  )
 
 export default CreateTodoProcedure
